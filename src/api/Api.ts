a762588abce44549511ea3e38bb16d927b62d676
Varad <varad.gcs@gmail.com>
2024-04-02 01:00:36 +0530

    Import UniqueId as deviceid from react-native-device-info

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,9 +1,19 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { getApiConfig } from './ApiConfig';
 
+import DeviceInfo from 'react-native-device-info';
+
+
+const getAndroidId = () => {
+  const androidId = DeviceInfo.getUniqueId();
+  return androidId;
+};
+
 // Function to make a GET request to apiUrl/auth with a custom header
-export const getAuthData = async (deviceId: string): Promise<AxiosResponse | null> => {
+export const getAuthData = async (): Promise<AxiosResponse | null> => {
   try {
+    const androidId = getAndroidId();
+
     // Get API configuration
     const apiConfig = await getApiConfig();
     if (!apiConfig) {
@@ -14,7 +24,7 @@ export const getAuthData = async (deviceId: string): Promise<AxiosResponse | nul
     // Set up the request headers
     const headers = {
       'Content-Type': 'application/json',
-      DEVICEID: deviceId,
+      DEVICEID: androidId,
     };
 
     // Set up the request config
@@ -26,6 +36,7 @@ export const getAuthData = async (deviceId: string): Promise<AxiosResponse | nul
 
     // Make the API call
     const response = await axios(config);
+    console.log(response)
     return response;
   } catch (error) {
     console.error('Error making GET request:', error);
